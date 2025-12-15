ab380b28527ffb6c86d4098e9f4cbec3cef2547e
23120018 <thienan0835@gmail.com>
2025-12-15 15:42:25 +0700

    feat(card): enhance MovieCard rendering and props handling

--- 23120018_BTCN2/src/components/MovieCard.jsx
+++ 23120018_BTCN2/src/components/MovieCard.jsx
@@ -1,3 +1,39 @@
-export const MovieCard = () => {
-  return <div>Movie Card - Nội dung đang được phát triển</div>;
+import React from 'react';
+
+export function MovieCard({ movie }) {
+  const posterSrc = movie.image || 'https://via.placeholder.com/300x450?text=No+Poster'
+
+  return (
+    <div className="w-1/3 cursor-pointer group">
+        {/* Container */}
+        <div className="rounded-lg overflow-hidden shadow-lg border border-gray-700 h-96 transition-transform duration-300 hover:scale-[1.25]">
+            {/* Poster Phim */}
+            <img
+                src={posterSrc}
+                alt={movie?.title || 'Movie Poster'}
+                className="w-full h-full object-fill"
+                loading="lazy"
+            />
+
+            {/* Tiêu đề Phim */}
+            <div 
+                // Vị trí tuyệt đối, nằm ở dưới cùng của container poster
+                className="absolute bottom-0 left-0 right-0 p-4 
+                            bg-black bg-opacity-75 text-white 
+                            transform translate-y-full transition-all duration-300 ease-in-out 
+                            opacity-0 
+                            group-hover:translate-y-0 group-hover:opacity-100" 
+            >
+                <p className="text-xl font-bold leading-tight">
+                    {movie?.title || 'Untitled Movie'}
+                </p>
+                {movie.year && (
+                  <p className="text-sm font-light mt-1">
+                      {movie.year}
+                  </p>
+                )}
+            </div>
+        </div>
+    </div>
+  );
 };
